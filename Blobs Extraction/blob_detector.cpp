06dff6be3db8548f06f92f137024086deb7bb973
brunoyudy <49072387+brunoyudy@users.noreply.github.com>
2020-06-01 20:15:15 -0300

    Add files via upload

--- Blobs Extraction/blob_detector.cpp
+++ Blobs Extraction/blob_detector.cpp
@@ -38,14 +38,31 @@ Mat kmeans(Mat input_image)
     return output;
 }
 
-Mat Blobs(Mat image)
+
+
+Mat Blobs(Mat image,int minThresh,int maxThresh, int minArea,int maxArea,int minCirc,int maxCirc,int minConvex,int maxConvex ,int minInertia,int maxInertia)
 {
-    Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create();
+    SimpleBlobDetector::Params params;
+    params.minThreshold = minThresh;
+    params.maxThreshold = maxThresh;
+    params.filterByArea = true;
+    params.minArea = minArea;
+    params.maxArea = maxArea;
+    params.filterByCircularity = true;
+    params.minCircularity = minCirc/100;
+    params.maxCircularity = maxCirc/100;
+    params.filterByConvexity = true;
+    params.minConvexity = minConvex/100;
+    params.maxConvexity = maxConvex/100;
+    params.filterByInertia = true;
+    params.minInertiaRatio = minInertia/100;
+    params.maxInertiaRatio = maxInertia/100;
+    Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
     std::vector<KeyPoint> keypoints;
     detector->detect( image, keypoints );
     Mat output;
     drawKeypoints(image, keypoints, output, Scalar(255,0,0), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-    cout << keypoints.size() << " Objects Detected." << endl;
+    //cout << keypoints.size() << " Objects Detected." << endl;
     return output;
 }
 
@@ -53,8 +70,40 @@ int main(int argc, const char** argv)
 {
     Mat input_image = imread(argv[1], 1);
     Mat binary_image = kmeans(input_image);
-    Mat output = Blobs(binary_image);
-    imshow("Input", input_image);
-    imshow("Output", output);
-    waitKey(0);
+
+    namedWindow("My Window", 1);
+    int minThreshold = 0;
+    int maxThreshold = 0;
+    int minCirc = 0;
+    int maxCirc = 0;
+    int minConvex = 0;
+    int maxConvex = 0;
+    int minInertia = 0;
+    int maxInertia = 0;
+    int minArea = 0;
+    int maxArea = 0;
+
+
+    while(true)
+    {
+        createTrackbar("Min Threshold", "My Window", &minThreshold, 255);
+        createTrackbar("Max Threshold", "My Window", &maxThreshold, 255);
+        createTrackbar("Min Circularity", "My Window", &minCirc, 100);
+        createTrackbar("Max Circularity", "My Window", &maxCirc, 100);
+        createTrackbar("Min Convexity" , "My Window" , &minConvex, 100);
+        createTrackbar("Max Convexity", "My Window" , &maxConvex, 100);
+        createTrackbar("Min Inertia", "My Window", &minInertia, 100);
+        createTrackbar("Max Inertia", "My Window", &maxInertia, 100);
+        createTrackbar("Min Area", "My Window", &minArea, 5000);
+        createTrackbar("Max Area", "My Window", &maxArea, 5000);
+        Mat output = Blobs(binary_image, minThreshold, maxThreshold, minArea, maxArea, minCirc, maxCirc, minConvex, maxConvex, minInertia, maxInertia);
+        imshow("Input", input_image);
+        imshow("Output", output);
+        int iKey = waitKey(50);
+        if(iKey==27)
+        {
+            break;
+        }
+    }
+    return 0;
 }
--- Blobs Extraction/blob_detector2.py
+++ Blobs Extraction/blob_detector2.py
@@ -2,25 +2,52 @@ from k_means import thresholding
 import cv2
 import numpy as np
 
-im = cv2.imread('image2.png', 1)
-im = thresholding(im)
+img = cv2.imread('image2.png', 1)
 
-params = cv2.SimpleBlobDetector_Params()
-params.filterByColor = False
-params.minThreshold = 65
-params.maxThreshold = 93
-params.blobColor = 0
-params.minArea = 10
-params.maxArea = 5000
-params.filterByCircularity = True
-params.filterByConvexity = True
-params.minConvexity = 0
-params.maxConvexity = 1.0
-params.minCircularity = 0
-params.maxCircularity = 1.0
-detector = cv2.SimpleBlobDetector_create(params)
-keypoints = detector.detect(im)
-counter = len(keypoints)
-im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-cv2.imshow("{} Objects detected".format(counter), im_with_keypoints)
-cv2.waitKey(0)
+def nothing(x):
+    pass
+
+cv2.namedWindow('image')
+
+cv2.createTrackbar('MinArea', 'image', 0, 10000, nothing)
+cv2.createTrackbar('MaxArea', 'image', 0, 10000, nothing)
+cv2.createTrackbar('MinConvexity', 'image', 0, 100, nothing)
+cv2.createTrackbar('MaxConvexity', 'image', 0, 100, nothing)
+cv2.createTrackbar('MinCircularity', 'image', 0, 100, nothing)
+cv2.createTrackbar('MaxCircularity', 'image', 0, 100, nothing)
+cv2.createTrackbar('MinThreshold', 'image', 0, 255, nothing)
+cv2.createTrackbar('MaxThreshold', 'image', 0, 255, nothing)
+'''
+cv2.createTrackbar('MinInertiaRatio', 'image', 0, 100, nothing)
+cv2.createTrackbar('MaxInterniaRatio', 'image', 0, 100, nothing)
+'''
+
+while(1):
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByColor = False
+    params.blobColor = 0
+    params.filterByCircularity = True
+    params.filterByConvexity = True
+    #params.filterByInertia = True
+    params.minArea = cv2.getTrackbarPos('MinArea', 'image')
+    params.maxArea = cv2.getTrackbarPos('MaxArea', 'image')
+    params.minConvexity = cv2.getTrackbarPos('MinConvexity', 'image')/100
+    params.maxConvexity = cv2.getTrackbarPos('MaxConvexity', 'image')/100
+    params.minCircularity = cv2.getTrackbarPos('MinCircularity', 'image')/100
+    params.maxCircularity = cv2.getTrackbarPos('MaxCircularity', 'image')/100
+    params.minThreshold = cv2.getTrackbarPos('MinThreshold', 'image')
+    params.maxThreshold = cv2.getTrackbarPos('MaxThreshold', 'image')
+    '''
+    params.minInertiaRatio = cv2.getTrackbarPos('MinInertiaRatio', 'image')/100
+    params.maxInertiaRatio = cv2.getTrackbarPos('MaxInertiaRatio', 'image')/100
+    '''
+    detector = cv2.SimpleBlobDetector_create(params)
+    keypoints = detector.detect(img)
+    counter = len(keypoints)
+    im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv2.imshow('Blobs', im_with_keypoints)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
